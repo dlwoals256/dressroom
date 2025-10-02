216b8d9f23dc383ad1cb4258e97b448a6e92ccf1
dlwoals256 <bocchithecoding@gmail.com>
2025-10-02 21:02:48 +0900

    [dev] bug fix

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,10 +3,8 @@ from .views import (
     UserRequestView,
 )
 from .views import devView
-from .views import CustomLoginView
 
 urlpatterns = [
     path('generate/', UserRequestView.as_view(), name='generate'),
     path('dev/', devView.as_view(), name='dev'),
-    path('auth/token/', CustomLoginView.as_view(), name='obtain-auth-token'),
 ]
